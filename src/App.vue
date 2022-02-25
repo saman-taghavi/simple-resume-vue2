b60ad524d33319471c536eb2089524711536a3cb
Saman Taghavi <samantaghavisani@gmail.com>
2022-02-25 19:41:14 +0330

    Add top part

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,36 @@
 <template>
-  <v-app> <v-main> </v-main> </v-app>
-</template>
+  <v-app>
+    <v-main>
+      <v-card CL color="blue" flat>
+        <v-card-title class="d-flex justify-center px-0" primary-title>
+          <v-avatar class="rounded-circle" color="grey" size="164" tile>
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"
+            ></v-img>
+          </v-avatar>
+        </v-card-title>
+        <v-card-text class="text-center mx-auto">
+          <h4 class="text-h4 white--text">{{ name }}</h4>
+          <h5 class="mt-5 text-subtitle-2">
+            {{ shortInfo }}
+          </h5>
+        </v-card-text>
+      </v-card>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
+      <v-container fluid> </v-container>
+    </v-main>
+  </v-app>
+</template>
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      name: "saman taghavi",
+      shortInfo: "I love football, tech and fantasy novels",
+    };
+  },
+};
+</script>
+
+<style></style>
